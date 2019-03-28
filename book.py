e1f74dab5f0eb620ddc78ec30fe1e6fcc7622507
coatlee <lbbas@126.com>
2019-03-28 09:48:55 +0800

    v1.0

--- book.py
+++ book.py
@@ -10,7 +10,7 @@ class Book:
         # 初始化空DataFrame用于后期汇总（有没有更简单的办法？）
         series = np.zeros((2, 65))
         self.dataSum = pd.DataFrame(series, index=["L", "N"], columns=range(5, 70))
-        self.dataComp = pd.DataFrame(series, index=["L", "N"], columns=range(5, 70))
+        self.datacheck = pd.DataFrame(series, index=["L", "N"], columns=range(5, 70))
 
     # 读取配置文件
     # filenames:[filename1,filename2...] type:list
@@ -24,8 +24,7 @@ class Book:
             filenames.append(line)
         return filenames
 
-    # 打开一个文件，并返回这个文件的文件名和第一个sheet
-    # sheetSet:[filename,sheet]  type:list
+    # 打开一个文件，并返回这个文件openpyxl对象和第一个sheet
     def openBook(self, filename):
         xlsbook = xl.load_workbook(filename)
         xlsheets = xlsbook.get_sheet_names()
@@ -62,22 +61,25 @@ class Book:
             book, sheet = self.openBook(filename)
             dataSet = self.readData(sheet)
             dataFrame = pd.DataFrame(dataSet, index=["L", "N"])
-            self.dataSum += dataFrame
+            self.datacheck += dataFrame
         sumBook, sumSheet = self.openBook(filenames[-1])
         sumDataSet = self.readData(sumSheet)
         sumDataFram = pd.DataFrame(sumDataSet, index=["L", "N"])
-        check = sumDataFram.sub(self.dataSum)
+        check = sumDataFram.sub(self.datacheck)
         check = check.fillna(0.0)
-        flags = check.any(axis=0)
-        flagcollects = []
-        # for index, flag in enumerate(flags):
-        #     print(index, flag)
-        #     if flag == True:
-        #         flagcollect = [index, flag]
-        #         flagcollects.append(flag)
-        #     else:
-        #         continue
-        # print(flagcollects)
+        flag = check.any(axis=None)
+        flags = []
+        if flag:
+            flagdict = check.any(axis=0).to_dict()
+            for key, value in flagdict.items():
+                if value:
+                    flags.append(key)
+                else:
+                    continue
+            print("以下行数据存在问题：", flags)
+        else:
+            print("数据完整性校验通过")
+
 
     # Case 1
     # 汇总所有分表的数据
@@ -92,6 +94,7 @@ class Book:
         sumBook, sumSheet = self.openBook(filenames[-1])
         self.bookfill(sumSheet, self.dataSum)
         sumBook.save(filenames[-1])
+        print("表格汇总完成，将进行数据完整性检查")
         return
 
     # Case 2
@@ -113,9 +116,25 @@ class Book:
             dataFrameFix = dataFrame.mul(rate)
             self.bookfill(sheet, dataFrameFix)
             book.save(filename)
+        print("表格修改完成，将进行数据完整性检查")
+        return
 
 
-book = Book()
-# book.bookSum(book.filenames)
-# book.bookFix(book.filenames)
-book.dataCheck(book.filenames)
+if __name__ == '__main__':
+    print("-----------------------------------------------------------------------------")
+    print("----------开始之前请确保已完成ini.txt文件存在、文件名正确、对应文件存在----------")
+    print("1.汇总分地区表")
+    print("2.修改分地区表")
+    print("3.汇总校验")
+    book = Book()
+    c = eval(input("请选择操作项:"))
+    if c == 1:
+        book.bookSum(book.filenames)
+        book.dataCheck(book.filenames)
+    elif c == 2:
+        book.bookFix(book.filenames)
+        book.dataCheck(book.filenames)
+    elif c == 3:
+        book.dataCheck(book.filenames)
+    else:
+        print("嘿，大兄弟。你这干啥呢？")
