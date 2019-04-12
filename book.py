f8d7d8d9c9e765802c3e5ff500551d8fbe9ef512
MrBeike <lbbas@126.com>
2019-04-12 10:15:44 +0800

    It's End,cause it's useless

--- book.py
+++ book.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-#for Mrs.jiang
+# for Mrs.jiang
 
 import openpyxl as xl
 import numpy as np
@@ -11,7 +11,7 @@ class Book:
         self.filenames = self.readIni()
         # 初始化空DataFrame用于后期汇总（有没有更简单的办法？）
         series = np.zeros((2, 65))
-        #L N 分别表示2018年数据分子、分母。
+        # L N 分别表示2018年数据分子、分母。
         self.dataSum = pd.DataFrame(series, index=["L", "N"], columns=range(5, 70))
         self.datacheck = pd.DataFrame(series, index=["L", "N"], columns=range(5, 70))
 
@@ -19,7 +19,7 @@ class Book:
     # filenames:[filename1,filename2...] type:list
     def readIni(self):
         filenames = []
-        file = open("ini.txt", "r")
+        file = open("ini.txt", "r", encoding='UTF-8')
         lines = file.readlines()
         # 获取每一行的文件名，设定最后一行为汇总表格
         for line in lines:
@@ -30,8 +30,8 @@ class Book:
     # 打开一个文件，并返回这个文件openpyxl对象和第一个sheet
     def openBook(self, filename):
         xlsbook = xl.load_workbook(filename)
-        xlsheets = xlsbook.get_sheet_names()
-        sheet = xlsbook.get_sheet_by_name(xlsheets[0])
+        xlsheets = xlsbook.sheetnames
+        sheet = xlsbook[xlsheets[0]]
         return xlsbook, sheet
 
     # 读取excel表格数据
@@ -83,7 +83,6 @@ class Book:
         else:
             print("数据完整性校验通过")
 
-
     # Case 1
     # 汇总所有分表的数据
     # data:pd.DataFrame({5:XX,6:xx,....})  type:pd.DataFrame.object
@@ -122,6 +121,24 @@ class Book:
         print("表格修改完成，将进行数据完整性检查")
         return
 
+    # Case3
+    # Specify for Mrs.jiang.Although this project is all for her,she never know—.—
+    def bookJiang(self, filenames):
+        for filename in filenames[:-2]:
+            book, sheet = self.openBook(filename)
+            dataSet = self.readData(sheet)
+            dataFrame = pd.DataFrame(dataSet, index=["L", "N"])
+            self.dataSum += dataFrame
+        sumBook, sumSheet = self.openBook(filenames[-2])
+        sumDataSet = self.readData(sumSheet)
+        sumDataFram = pd.DataFrame(sumDataSet, index=["L", "N"])
+        blankDataFram = sumDataFram - self.dataSum
+        blankBook, blankSheet = self.openBook(filenames[-1])
+        self.bookfill(blankSheet, blankDataFram)
+        blankBook.save(filenames[-1])
+        print("表格修改完成,请查看")
+        return
+
 
 if __name__ == '__main__':
     print("-----------------------------------------------------------------------------")
@@ -129,8 +146,9 @@ if __name__ == '__main__':
     print("1.汇总分地区表")
     print("2.修改分地区表")
     print("3.汇总校验")
-    book = Book()
+    print("4.蒋老师定制")
     c = eval(input("请选择操作项:"))
+    book = Book()
     if c == 1:
         book.bookSum(book.filenames)
         book.dataCheck(book.filenames)
@@ -139,5 +157,7 @@ if __name__ == '__main__':
         book.dataCheck(book.filenames)
     elif c == 3:
         book.dataCheck(book.filenames)
+    elif c == 4:
+        book.bookJiang(book.filenames)
     else:
         print("嘿，大兄弟。你这干啥呢？")
